c173a3e284554bf38b34a7f42b0e25b8169962a1
Wxx <2563806166@qq.com>
2024-06-04 02:57:43 +0800

    version2

---
+++ version2/src/main/java/part2/Client/proxy/ClientProxy.java
@@ -0,0 +1,42 @@
+package part2.Client.proxy;
+
+
+import part2.Client.rpcClient.RpcClient;
+import part2.Client.rpcClient.impl.NettyRpcClient;
+import part2.common.Message.RpcRequest;
+import part2.common.Message.RpcResponse;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author wxx
+ * @version 1.0
+ * @create 2024/2/6 16:49
+ */
+public class ClientProxy implements InvocationHandler {
+    //传入参数service接口的class对象，反射封装成一个request
+
+    private RpcClient rpcClient;
+    public ClientProxy() throws InterruptedException {
+        rpcClient=new NettyRpcClient();
+    }
+
+    //jdk动态代理，每一次代理对象调用方法，都会经过此方法增强（反射获取request对象，socket发送到服务端）
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        //构建request
+        RpcRequest request=RpcRequest.builder()
+                .interfaceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .params(args).paramsType(method.getParameterTypes()).build();
+        //数据传输
+        RpcResponse response= rpcClient.sendRequest(request);
+        return response.getData();
+    }
+     public <T>T getProxy(Class<T> clazz){
+        Object o = Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, this);
+        return (T)o;
+    }
+}
