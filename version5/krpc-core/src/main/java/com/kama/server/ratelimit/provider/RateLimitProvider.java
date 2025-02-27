bbe88333df69aac29763c2cb7dbd6f508135f62e
Wxx <2563806166@qq.com>
2025-02-27 16:21:48 +0800

    fix

--- version5/krpc-core/src/main/java/com/kama/server/ratelimit/provider/RateLimitProvider.java
+++ version5/krpc-core/src/main/java/com/kama/server/ratelimit/provider/RateLimitProvider.java
@@ -20,13 +20,13 @@ public class RateLimitProvider {
     private final Map<String, RateLimit> rateLimitMap = new ConcurrentHashMap<>();
 
     // 默认的限流桶容量和令牌生成速率
-    private static final int DEFAULT_CAPACITY = 100;
-    private static final int DEFAULT_RATE = 10;
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_RATE = 100;
 
     // 提供限流实例
     public RateLimit getRateLimit(String interfaceName) {
         return rateLimitMap.computeIfAbsent(interfaceName, key -> {
-            RateLimit rateLimit = new TokenBucketRateLimitImpl(DEFAULT_CAPACITY, DEFAULT_RATE);
+            RateLimit rateLimit = new TokenBucketRateLimitImpl(DEFAULT_RATE, DEFAULT_CAPACITY);
             log.info("为接口 [{}] 创建了新的限流策略: {}", interfaceName, rateLimit);
             return rateLimit;
         });
